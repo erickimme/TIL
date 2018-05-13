25c9d5307260d6a4551b4460b652a0cb7f0a80a4
JaeYeon <kim802@purdue.edu>
2018-05-13 19:18:28 -0400

    day1 programmers getMiddle

--- CodingInterviewPrep/java_programmersCoKr/src/StringExercise.java
+++ CodingInterviewPrep/java_programmersCoKr/src/StringExercise.java
@@ -1,3 +1,11 @@
+/*
+getMiddle메소드는 하나의 단어를 입력 받습니다.
+단어를 입력 받아서 가운데 글자를 반환하도록 getMiddle메소드를 만들어 보세요.
+단어의 길이가 짝수일경우 가운데 두글자를 반환하면 됩니다.
+예를들어 입력받은 단어가 power이라면 w를 반환하면 되고, 입력받은 단어가 test라면 es를 반환하면 됩니다.
+
+https://programmers.co.kr/learn/challenge_codes/82
+ */
 public class StringExercise{
     String getMiddle(String word){
         int len = word.length();
